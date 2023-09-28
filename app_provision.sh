e9e27a60f2acd6f5c7b3a4a6900daf08f4cd7691
Luke Weller <lp.weller01@gmail.com>
2023-09-28 16:08:55 +0100

    App Provision Automation

---
+++ app_provision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt upgrade -y
+
+sudo apt install nginx -y
+
+sudo systemctl restart nginx
+
+sudo systemctl enable nginx
+
+sudo apt install git-all -y
+
+git clone https://github.com/LSF970/sparta_test_app.git
+
+cd sparta_test_app
+
+cd app
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt install nodejs -y
+
+sudo npm install pm2 -g
+
+npm install
+
+node app.js
