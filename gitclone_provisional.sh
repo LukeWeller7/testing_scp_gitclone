ad6303dca4fd03dd9ac885783b207f40f4b8811b
Luke Weller <lp.weller01@gmail.com>
2023-10-11 15:37:46 +0100

    Testing Webhook

--- gitclone_provisional.sh
+++ gitclone_provisional.sh
@@ -10,21 +10,19 @@ sudo systemctl restart nginx
 
 sudo systemctl enable nginx
 
-sudo apt install git-all -y
+sudo apt install git -y
 
-git clone https://github.com/LukeWeller7/testing_scp_gitclone
-
-cd testing_scp_gitclone
-
-cd app
-
-cd app
+git clone https://github.com/LukeWeller7/testing_scp_gitclone.git
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 sudo apt install nodejs -y
 
 sudo npm install pm2 -g
+
+cd testing_scp_gitclone
+
+cd app
 
 npm install
 
---
+++ testing_webhook.md
@@ -0,0 +1,1 @@
+The purpose of this file is to test out the connection to Jenkins and Github via a webhook
